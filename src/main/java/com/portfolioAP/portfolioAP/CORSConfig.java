089c29c7c0f273a297dd69af12bbba030077c0df
juan <roldanjuan2003@gmail.com>
2023-03-15 11:41:48 -0300

    mod cors

--- src/main/java/com/portfolioAP/portfolioAP/CORSConfig.java
+++ src/main/java/com/portfolioAP/portfolioAP/CORSConfig.java
@@ -3,19 +3,18 @@ package com.portfolioAP.portfolioAP;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
-public class CORSConfig {
-
+public class CORSConfig implements WebMvcConfigurer{
     @Bean
     public WebMvcConfigurer CORSConfigurer(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT","DELETE");
+                registry.addMapping("/**");
             }
         };
     }
